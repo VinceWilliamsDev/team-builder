55c2b0f684c1962dd5e60683401e605ac291fba4
Vince Williams <williamsvm20@gmail.com>
2020-06-17 00:43:43 -0400

    set up state hook for the team object and initial values

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState }from 'react';
 import './App.css';
+import { v4 as uuid } from 'uuid'
 
 function App() {
+  
+  const initialTeam = [
+    { id: uuid(),
+      name: 'Bob',
+      email: 'bob@bob.com',
+      role: 'Engineer'
+    }
+  ]
+  
+  const formValues = {
+    name: '',
+    email: '',
+    role: ''
+  }
+
+  const { team, setTeam } = useState(initialTeam)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
